222cdfd5a1277d15b47e8664b6e1a85aa339f268
전민수 <minsu2530@u.sogang.ac.kr>
2021-01-06 22:03:01 +0900

    404 오류 등록

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -13,6 +13,8 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
+def page_not_found(e):
+    return render_template('404.html'),404
 
 def create_app():
     app = Flask(__name__)
@@ -23,7 +25,7 @@ def create_app():
         migrate.init_app(app, db, render_as_batch=True)
     else:
         migrate.init_app(app, db)
-
+    app.register_error_handler(404, page_not_found)
     from . import models
     from .views import main_views, question_views, answer_views, auth_views, comment_views, vote_views
     app.register_blueprint(main_views.bp)
