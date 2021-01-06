b6b773bf31f568dbcde6aed0eb3483682e2affb0
전민수 <minsu2530@u.sogang.ac.kr>
2021-01-06 17:06:29 +0900

    123

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
-Base_Dir = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(os.path.join(Base_Dir,"pybo.db"))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 SECRET_KEY = "dev"
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -13,39 +13,42 @@ bp = Blueprint('question',__name__,url_prefix='/question')
 def _list():
     page = request.args.get('page',type=int,default=1)
     kw = request.args.get('kw', type=str, default='')
-    so = request.args.get('so', type=str, default='')
+    so = request.args.get('so', type=str, default='recent')
 
+    # 정렬
     if so == 'recommend':
-        subquery = db.session.query(question_voter.c.question_id, func.count('*').label('num_voter')) \
+        sub_query = db.session.query(question_voter.c.question_id, func.count('*').label('num_voter')) \
             .group_by(question_voter.c.question_id).subquery()
         question_list = Question.query \
-            .outerjoin(subquery, Question.id==subquery.c.question_id) \
-            .order_by(subquery.c.num_voter.desc(), Question.create_date.desc())
+            .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
+            .order_by(sub_query.c.num_voter.desc(), Question.create_date.desc())
     elif so == 'popular':
-        subquery = db.session.query(Answer.question_id, func.count('*').label('num_answer')) \
+        sub_query = db.session.query(Answer.question_id, func.count('*').label('num_answer')) \
             .group_by(Answer.question_id).subquery()
         question_list = Question.query \
-            .outerjoin(subquery, Question.id == subquery.c.question_id) \
-            .order_by(subquery.c.num_answer.desc(), Question.create_date.desc())
-    else:
+            .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
+            .order_by(sub_query.c.num_answer.desc(), Question.create_date.desc())
+    else:  # recent
         question_list = Question.query.order_by(Question.create_date.desc())
 
     #조회
     if kw:
         search = '%%{}%%'.format(kw)
-        subquery = db.session.query(Answer.question_id, Answer.content, User.user_name) \
+        sub_query = db.session.query(Answer.question_id, Answer.content, User.username) \
             .join(User, Answer.user_id == User.id).subquery()
         question_list = question_list \
             .join(User) \
-            .outerjoin(subquery, subquery.c.question_id == Question.id) \
-            .filter(Question.subject.ilike(search)  |
-                    Question.content.ilike(search)  |
-                    User.user_name.ilike(search)    |
-                    subquery.c.content.ilike(search)|
-                    subquery.c.user_name.ilike(search)
-                    )\
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id) \
+            .filter(Question.subject.ilike(search) |  # 질문제목
+                    Question.content.ilike(search) |  # 질문내용
+                    User.username.ilike(search) |  # 질문작성자
+                    sub_query.c.content.ilike(search) |  # 답변내용
+                    sub_query.c.username.ilike(search)  # 답변작성자
+                    ) \
             .distinct()
-    question_list = question_list.paginate(page,per_page=10)
+
+    # 페이징
+    question_list = question_list.paginate(page, per_page=10)
     return render_template('question/question_list.html',question_list=question_list, page=page, kw=kw, so=so);
 
 @bp.route('/detail/<int:question_id>/')
