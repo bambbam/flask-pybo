825b36cef173d99e858cb747fb923d413de461b1
전민수 <minsu2530@u.sogang.ac.kr>
2021-01-07 14:19:17 +0900

    add logger

--- config/production.py
+++ config/production.py
@@ -1,7 +1,30 @@
-#환경변수 모아두는 곳(서버환경)
-
+# 환경변수 모아두는 곳(서버환경)
+from logging.config import dictConfig
 from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\x8d\xc6\xa8\r\xcf\xeeR\xba}\x00k\x98\xbe\xd6K8'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'Filename': os.path.join(BASE_DIR, 'logs/pyboProject.log'),
+            'maxBytes': 1024 * 1024 * 5,
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
